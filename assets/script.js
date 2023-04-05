e5fdbaa833fdffc7c49cffebf9ef272b16a9aff9
vincenzodivita <divitavincenzo@icloud.com>
2023-04-05 14:45:26 +0200

    Add prompt for ask train distance

--- assets/script.js
+++ assets/script.js
@@ -4,3 +4,15 @@ il prezzo del biglietto è definito in base ai km (0.21 € al km)
 va applicato uno sconto del 20% per i minorenni
 va applicato uno sconto del 40% per gli over 65.
 L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca. */
+
+// Chiedere all'utente i km da percorrere
+const trainDistance = prompt("Distanza da percorrere in treno?")
+
+console.log("Train distance: " + trainDistance);
+// Chiedere età dell'utente
+
+// Calcolare prezzo del biglietto (0.21 €/km)
+
+// Calcolare sconto in base all'età (20% per minorenni e 40% per over 65)
+
+// Calcolare prezzo finale
